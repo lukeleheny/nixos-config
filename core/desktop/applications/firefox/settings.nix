84201ec60040164068eb4eb618306a88e1971d29
Luke Leheny <hello@lukeleheny.com>
2026-08-31 08:02:48 -0400

    Use about:newtab as new tab page

--- core/desktop/applications/firefox/settings.nix
+++ core/desktop/applications/firefox/settings.nix
@@ -45,7 +45,7 @@
       "browser.profiles.enabled" = false;
       "browser.search.suggest.enabled.private" = true;
       "browser.sessionstore.restore_pinned_tabs_on_demand" = true;
-      "browser.startup.homepage" = "chrome://browser/content/blanktab.html";
+      "browser.startup.homepage" = "about:newtab";
       "browser.startup.page" = 3;
       "browser.tabs.firefox-view" = false;
       "browser.tabs.insertAfterCurrent" = false;
