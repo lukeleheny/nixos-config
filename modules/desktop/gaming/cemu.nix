bf12ece6d6b78c4bf25a8b2d35fce1edd4e78615
Luke Leheny <hello@lukeleheny.com>
2026-09-06 12:21:46 -0400

    Re-enable BOTW backups

--- modules/desktop/gaming/cemu.nix
+++ modules/desktop/gaming/cemu.nix
@@ -29,25 +29,25 @@
     categories = [ "Game" ];
   };
 
-  # systemd.services.botw-backup = {
-  #   description = "BOTW save backup";
-  #   path = with pkgs; [ gnutar gzip ];
-  #   script = ''
-  #     cd /home/luke/.local/share/Cemu/mlc01/usr/save/00050000/101c9400/user/80000001
-  #     tar -czf "/home/luke/Backups/BOTW/$(date +%Y-%m-%d).tar.gz" *
-  #     ls -F /home/luke/Backups/BOTW/*.tar.gz | head -n -10 | xargs -r rm
-  #   '';
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     User = "luke";
-  #   };
-  # };
-  # systemd.timers.botw-backup = {
-  #   wantedBy = [ "timers.target" ];
-  #   timerConfig = {
-  #     OnCalendar = "*-*-* 04:00:00";
-  #     Persistent = true;
-  #     Unit = "botw-backup.service";
-  #   };
-  # };
+  systemd.services.botw-backup = {
+    description = "BOTW save backup";
+    path = with pkgs; [ gnutar gzip ];
+    script = ''
+      cd /home/luke/.local/share/Cemu/mlc01/usr/save/00050000/101c9400/user/80000001
+      tar -czf "/home/luke/Backups/BOTW/$(date +%Y-%m-%d).tar.gz" *
+      ls -F /home/luke/Backups/BOTW/*.tar.gz | head -n -10 | xargs -r rm
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      User = "luke";
+    };
+  };
+  systemd.timers.botw-backup = {
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "*-*-* 04:00:00";
+      Persistent = true;
+      Unit = "botw-backup.service";
+    };
+  };
 }
